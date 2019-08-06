20bbeab45f3e85b205199b0db0e0c8d53eade882
Dankarthik25 <dankarthik25@gmail.com>
2019-08-06 12:58:35 +0530

    Three type of constant Literal,keyword,#defined

--- CppNuts/005_sizeof_Operator.cpp
+++ CppNuts/005_sizeof_Operator.cpp
@@ -13,9 +13,24 @@ int main(int argc, char const *argv[])
     cout<<"long : "<<sizeof(long)<<endl;
     cout<<"long long : "<<sizeof(long long)<<endl;
     cout<<"============================"<<endl;
-
     cout<<"float : "<<sizeof(float)<<endl;
     cout<< "double :"<<sizeof(double)<<endl;
-    cout<< "long double : "<<sizeof(long double)<<endl;
+    cout<< "long double  : "<<sizeof(long double)<<endl;
+    cout<<"============================"<<endl;
+/*
+        <Climits>
+ */
+    cout<< "Minimum value : " <<endl;
+    cout<< "char : "<< WCHAR_MIN <<endl;
+    cout<< "int8 : "<<INT8_MIN<<endl;
+    // cout<< SHRT_MIN<<endl;
+/*
+        Operator sizeof
+ */
+    int age{21};
+    cout<< "age is "<< sizeof(age)<<" bytes"<<endl;
+    double wage{22.34};
+    cout<<"wage is " << sizeof(wage)<< " bytes"<<endl;
+
     return 0;
 }
