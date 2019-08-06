aa95b845a5768b56ef15764cbab78fab50c8aed2
Dankarthik25 <dankarthik25@gmail.com>
2019-08-06 13:05:04 +0530

    declaration, inicilisation, acessing  elements

---
+++ CppNuts/006_constant.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    // Types:
+        //1. Literal Constants
+    
+        //2. Declared Constants
+    const double pi{3.14515926};
+
+        //3. #define Constant
+
+    // #define pi 3.1415926;
+    return 0;
+}
