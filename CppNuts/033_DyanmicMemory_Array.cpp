dfb67be7bd2c13c344c1f42f8c73bc427c5092ac
Dankarthik25 <dankarthik25@gmail.com>
2019-08-10 09:06:45 +0530

    Example of Array impletation by pointers

--- CppNuts/033_DyanmicMemory_Array.cpp
+++ CppNuts/033_DyanmicMemory_Array.cpp
@@ -22,8 +22,37 @@ int main(int argc, char const *argv[])
 
     int *score_ptr{score};
 
-    cout<< score_ptr[0] <<endl;
-    cout<< score_ptr[1]<<endl;
-    cout<< score_ptr[2]<<endl;
-    return 0;
+    cout<<"0th element "<<score_ptr<<" value = "<<*(score_ptr)<<" or "<< score_ptr[0] <<endl;
+    cout<<"1th element "<<score_ptr +1<<" value = "<<*(score_ptr+1)<<" or "<< score_ptr[1]<<endl;
+    cout<<"2th element "<<score_ptr+2<<" value  = "<< *(score_ptr+2)<<" or "<<score_ptr[2]<<endl;
+
+/*
+Pointer arithmetic
+assignment expressions ;
+      ptr +1 (address + 1* size_variable )
+Comparission expression:
+        ptr1 == ptr2 // comparring address
+increment/decrement
+int_ptr++;
+int_ptr--;
+int_ptr = +n // int_ptr  = int_ptr + n*(sizeof(type))
+
+Substracting two pointers (No.of elements btw pointers):
+int n = int_ptr2 -int_ptr1;
+ */
+
+string s1{"Frank"};
+string s2{"Frank"};
+
+string *p1 {&s1};
+string *p2 {&s2};
+string *p3 {&s1};
+
+cout << (p1==p2)<<endl;
+cout<<(p1==p3)<<endl;
+
+cout << (*p1==*p2)<<endl;
+cout<<(*p1==*p3)<<endl;
+
+return 0;
 }
